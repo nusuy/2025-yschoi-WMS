8a4fe2534b0a7be5167131d773dc1f308ffed7da
nusuy <nsuy.ch@gmail.com>
2025-11-20 16:59:24 +0900

    feat: Add Interceptor Config (#5)

--- src/main/java/com/yschoi/demoapp/common/config/WebConfig.java
+++ src/main/java/com/yschoi/demoapp/common/config/WebConfig.java
@@ -2,15 +2,29 @@ package com.yschoi.demoapp.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {	
+	private final AuthInterceptor authInterceptor;
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOrigins("http://localhost:5173")
 				.allowedMethods("GET", "POST")
 				.allowedHeaders("*").maxAge(3000);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor)
+				.addPathPatterns("/**")
+				//.excludePathPatterns("/css/**", "/images/**", "/js/**", "/**/login/**");
+				.excludePathPatterns("/css/**", "/images/**", "/js/**", "/**/login/**", "/error");
 	}
 }
