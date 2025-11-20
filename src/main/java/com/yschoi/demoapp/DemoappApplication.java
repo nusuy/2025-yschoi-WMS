66f91557c9b75a3d115befeb54774d418f957190
nusuy <nsuy.ch@gmail.com>
2025-11-20 16:54:31 +0900

    chore: Add spring build config (#5)

--- src/main/java/com/yschoi/demoapp/DemoappApplication.java
+++ src/main/java/com/yschoi/demoapp/DemoappApplication.java
@@ -2,12 +2,30 @@ package com.yschoi.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+* @packageName   : com.yschoi.demoapp
+* @fileName      : DemoappApplication.java
+* @author        : YS CHOI
+* @date          : 2025.11.04
+* @description   : Application Starter
+* ===========================================================
+* DATE              AUTHOR             NOTE
+* -----------------------------------------------------------
+* 2025.11.04        YS CHOI       최초 생성
+*/
 
 @SpringBootApplication
-public class DemoappApplication {
+public class DemoappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoappApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoappApplication.class);
+    }
 }
